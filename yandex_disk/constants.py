af9e23b7994982f141a308d4e7a738d86664bae8
vensilya@gmail.com <vensilya@gmail.com>
2017-01-29 23:39:07 +0300

    Add yandex app secret

---
+++ yandex_disk/constants.py
@@ -0,0 +1,6 @@
+from yandex_disk.constants_local import MY_YANDEX_APP_ID, MY_YANDEX_APP_PASSWORD
+
+# your app id
+YANDEX_APP_ID = MY_YANDEX_APP_ID
+# your app password
+YANDEX_APP_PASSWORD = MY_YANDEX_APP_PASSWORD
