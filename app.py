94e4bbee55f9a10db053985d6ef15f529375188b
vensilya@gmail.com <vensilya@gmail.com>
2017-05-01 22:44:28 +0300

    Play media files from google drive

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from __future__ import print_function
+import httplib2
 import sys
 import os
+import urllib.request
 
 import dropbox
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
 from dropbox_manager.constants import DROPBOX_TOKEN
 from dropbox_manager.dropbox_manager import DropboxManager
@@ -12,6 +19,13 @@ from yandex_disk.yandex_disk_manager import YandexDiskManager
 MY_DROPBOX_APP_KEY = 'i58xh5qigf55ma1'
 MY_DROPBOX_APP_SECRET = 'a9iwynwq94nyuca'
 
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'CloudManager'
+
+
+def play_media_file(link):
+    os.system('gst-launch-1.0 playbin uri=%s >/dev/null' % link)
 
 def dropbox_obtain_access_token():
     flow = dropbox.client.DropboxOAuth2FlowNoRedirect(MY_DROPBOX_APP_KEY, MY_DROPBOX_APP_SECRET)
@@ -27,7 +41,42 @@ def play_file_from_dropbox(path):
     access_token = dropbox_obtain_access_token()
     dropbox_client = dropbox.dropbox.Dropbox(access_token)
     link = dropbox_client.files_get_temporary_link(path).link
-    os.system('gst-launch-1.0 playbin uri=%s >/dev/null' % link)
+    play_media_file(link)
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+    return credentials
+
+def get_link_to_file(files, filename):
+    print(filename)
+    for file in files:
+        print(file['name'])
+        if file['name'] == filename:
+            return file['webContentLink']
+    return ''
+
+def play_file_from_google(filename):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(fields='nextPageToken, files(name, webContentLink)').execute()
+    files = results.get('files', [])
+
+    link = get_link_to_file(files, filename)
+    play_media_file(link)
 
 def main():
     #dropbox = DropboxManager(DROPBOX_TOKEN)
@@ -41,6 +90,8 @@ def main():
     service = path.split('/')[1]
     if service == 'dropbox':
         play_file_from_dropbox(path[8:])
+    elif service == 'google':
+        play_file_from_google(path.split('/')[len(path.split('/')) - 1])
     # yandex_disk = YandexDiskManager(MY_YANDEX_TOKEN)
     # print(yandex_disk.files_list())
 
